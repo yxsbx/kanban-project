928e736118dae7fc8208fe0cd244e8a7fe6f0f23
Felipe Costa <felipe95176@gmail.com>
2024-10-18 20:16:39 -0300

    o Form.ts criará todo o html necessário

--- src/components/Form.ts
+++ src/components/Form.ts
@@ -1,29 +1,33 @@
 export default class FormComponent {
   constructor() {
-    const form = document.querySelector("#form-burger-container");
+    const app = document.querySelector("#app");
 
-    if (form) form.innerHTML = this.render();
+    if (app) app.innerHTML += this.render();
   }
 
   render() {
     return `
-            <form id="formCard" method="post">
-                <div class="add flex justify-center items-center">
-                    <select name="tag" id="tag">
-                        <option value="frontend" selected>Frontend</option>
-                        <option value="backend">Backend</option>
-                        <option value="data">Data</option>
-                        <option value="ux / ui">UX / UI</option>
-                    </select>
-                    <label for="name">Nome
-                        <input type="text" name="name" id="name"/>
-                    </label>
-                    <label for="description">Descrição
-                        <input type="text" name="description" id="description">
-                    </label>
-                    <button type="submit">Adicionar</button>
-                </div>
-            </form>
+        <div id="form-burger-container"></div>
+            <div class="container-fluid text-center" id="form-card-container"> 
+                <form id="formCard" method="post">
+                    <div class="add flex justify-center items-center">
+                        <select name="tag" id="tag">
+                            <option value="frontend" selected>Frontend</option>
+                            <option value="backend">Backend</option>
+                            <option value="data">Data</option>
+                            <option value="ux / ui">UX / UI</option>
+                        </select>
+                        <label for="name">Nome
+                            <input type="text" name="name" id="name"/>
+                        </label>
+                        <label for="description">Descrição
+                            <input type="text" name="description" id="description">
+                        </label>
+                        <button type="submit">Adicionar</button>
+                    </div>
+                </form>
+            </div>
+        <div id="cards-list" class="row"></div>
         `;
   }
 }
